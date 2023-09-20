401df89a69c51ad0bd815eabfe7912e297bb53b7
Ian Kenzo <137218678+ianfujii@users.noreply.github.com>
2023-09-19 22:13:42 -0300

    first commit

---
+++ lista de tarefas - exercicio/script.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#task-form").submit(function (e) {
+        e.preventDefault();
+        
+        const taskName = $("#task-input").val();
+        
+        if (taskName.trim() !== "") {
+            const listItem = $("<li>").text(taskName);
+            listItem.click(function () {
+                $(this).toggleClass("completed");
+            });
+            
+            $("#task-list").append(listItem);
+            $("#task-input").val("");
+        }
+    });
+});
